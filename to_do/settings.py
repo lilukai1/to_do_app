4d0bceceed788e89e128e7b063367656c69ed123
lilukai1 <50470636+lilukai1@users.noreply.github.com>
2020-11-03 23:02:14 -0600

    a

--- to_do/settings.py
+++ to_do/settings.py
@@ -1,8 +1,8 @@
-from to_do.config.base import *
+# from to_do.config.base import *
 
-# try:
-#     print("Trying dev settings...")
-#     from to_do.config.local import *
-# except ImportError:
+try:
+    print("Trying dev settings...")
+    from to_do.config.local import *
+except ImportError:
     print("Cant find local.  Trying deploy settings.")
     from to_do.config.deploy import *
