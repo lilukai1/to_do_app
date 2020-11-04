4ebcc6f3b543edb274f2c61175ca85f44c6c57f8
lilukai1 <50470636+lilukai1@users.noreply.github.com>
2020-11-03 22:37:42 -0600

    small syntax edit

--- to_do/config/deploy.py
+++ to_do/config/deploy.py
@@ -6,11 +6,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
-django_heroku.settings(locals())
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -22,3 +17,9 @@ INSTALLED_APPS = [
     'accounts',
     'crispy_forms',    
 ]
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
+django_heroku.settings(locals())
