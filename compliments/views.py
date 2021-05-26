3d36b064de813ef4b9eca19d42538e9986cd2b74
lilukai1 <50470636+lilukai1@users.noreply.github.com>
2021-05-26 07:36:00 -0500

    rest

--- compliments/views.py
+++ compliments/views.py
@@ -7,22 +7,23 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .serializers import ComplimentSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
-
-@method_decorator(csrf_exempt, name='dispatch')
-def compliment_list(request):
+@api_view(['GET', 'POST'])
+def compliment_list(request, format=None):
     """
     List all code compliments, or create a new compliment.
     """
     if request.method == 'GET':
         compliments = Compliments.objects.all()
         serializer = ComplimentSerializer(compliments, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ComplimentSerializer(data=data)
+        serializer = ComplimentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
